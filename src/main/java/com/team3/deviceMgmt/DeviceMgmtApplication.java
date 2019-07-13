313ad6efd2301cde69812003487db1a6e6d02022
raviteja <raviteja.koditiwada@gmail.com>
2019-07-13 12:22:15 +0530

    intial commit

---
+++ src/main/java/com/team3/deviceMgmt/DeviceMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.team3.deviceMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceMgmtApplication.class, args);
+	}
+
+}
